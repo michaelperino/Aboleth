94fe10205de3dd12d5559f60b1bcd3523e236a27
Michael Francis Perino <perino@purdue.edu>
2019-10-30 17:56:40 -0400

    Revised ability scores to be more centered on card

--- Aboleth.py
+++ Aboleth.py
@@ -3,6 +3,10 @@ from PIL import ImageFont,Image,ImageDraw
 import pprint
 import os
 import json
+
+monsters = "5e-SRD-Monsters.json"
+skilldoc = "5e-SRD-Ability-Scores.json"
+
 def main():
     jsondatabase = {}
     path = '../open5e/'
@@ -11,17 +15,16 @@ def main():
             with open(path+file, 'r') as f:
                 jsondatabase[file] = json.load(f)
     elementas = jsondatabase.keys()
-    print(list(elementas))
-    print(list(elementas)[0])
-    #print(jsondatabase['5e-SRD-StartingEquipment.json'])
+    skills = []
+    for element in jsondatabase[skilldoc]:
+        skills.append(element["full_name"])
     while True:
         stype = input()
         query = input()
         if stype == "stats" or stype == "stat":
-            for element in jsondatabase["5e-SRD-Monsters.json"]:
+            for element in jsondatabase[monsters]:
                 if element["name"] == query:
-                    skills = ['Strength','Dexterity','Constitution','Intelligence','Wisdom','Charisma']
-                    pprint.pprint(element, indent=2)
+                    #pprint.pprint(element, indent=2)
                     card = Image.open('./images/basecard.png')
                     cardplt = ImageDraw.Draw(card)
                     for skill in skills:
@@ -31,12 +34,18 @@ def main():
 def skilldraw(image,stat,index):
     font = ImageFont.truetype('arial.ttf',size=75)
     if(abs(stat/10) < 1):
-        (x,y)=(487+index*288,885)
+        (x,y)=(490+index*288,885)
     else:
         (x,y)=(467+index*288,885)
     color = 'rgb(0,0,0)'
     image.text((x,y),str(stat),fill=color,font=font)
-
+    font = ImageFont.truetype('arial.ttf',size=125)
+    stat = int((stat-10)/2)
+    if(stat < 0):
+        (x,y)=(455+index*288,715)
+    else:
+        (x,y)=(478+index*288,715)
+    image.text((x,y),str(stat),fill=color,font=font)
 main()
 
                 
