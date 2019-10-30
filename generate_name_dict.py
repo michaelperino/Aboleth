e23937c0413102be4b2893dc5fbb19144c7bf2e4
MichaelFrancisPerino <perino@purdue.edu>
2019-10-30 17:42:46 -0400

    Name List Working towards some sort of spell check and added way to list all names of monsters as such. Improved card script with the modifier now listed above ability raw score.

---
+++ generate_name_dict.py
@@ -0,0 +1,21 @@
+#monster names
+import os
+import json
+
+jsondatabase = {}
+path = '../open5e/'
+for file in os.listdir(path):
+    if file.endswith(".json"):
+        with open(path+file, 'r') as f:
+            jsondatabase[file] = json.load(f)
+elementas = list(jsondatabase.keys())
+names = {}
+for element in elementas:
+    index = 0
+    names[element] = []
+    if "name" in jsondatabase[element][0]:
+        for elementint in jsondatabase[element]:
+            names[element].append(elementint["name"])
+            index+=1
+with open("./namelist.json","w+") as namefile:
+    json.dump(names,namefile,sort_keys=True,indent=4, separators=(',', ': '))
